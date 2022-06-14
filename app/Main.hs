b6d4fb3b56cecc71831233aced0fb78f98fcae9a
vikstapolis <vikramaunnikrishnan@gmail.com>
2022-06-14 12:57:54 +0530

    Created initial blog with test post

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+
 module Main where
 
+import Hakyll.Main (hakyll)
+
+import Hakyll.Core.Rules (match, route, compile, create)
+import Hakyll.Core.Routes (idRoute, setExtension)
+import Hakyll.Core.Compiler (makeItem, getResourceBody, loadAll)
+
+import Hakyll.Web.Template (loadAndApplyTemplate, templateBodyCompiler)
+import Hakyll.Web.Template.Context (defaultContext, listField, constField)
+import Hakyll.Web.Template.List (recentFirst)
+
+import Hakyll.Web.Html.RelativizeUrls (relativizeUrls)
+
+import Hakyll.Web.CompressCss (compressCssCompiler)
+import Hakyll.Web.Pandoc (pandocCompiler)
+
+import Hakyll.Images (loadImage)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = hakyll do
+    match "index.html" do 
+        route idRoute
+        compile $
+            let ctx = constField "title" "Home" <> defaultContext
+            in getResourceBody
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
+
+    create ["archive.html"] do
+        route idRoute
+        compile do
+            posts <- recentFirst =<< loadAll "posts/*"
+
+            let archiveCtx = listField "posts" defaultContext (pure posts)
+                            <> constField "title" "Archives"
+                            <> defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+    match "contact.md" do
+        route $ setExtension "html"
+        compile $
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/default.html" (constField "title" "Contact me" <> defaultContext)
+
+    match "posts/*.md" do
+        route $ setExtension "html"
+        compile $
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext 
+                >>= relativizeUrls
+
+    match "images/*.png" do
+        route idRoute
+        compile loadImage
+
+    match "styles.css" do
+        route idRoute
+        compile compressCssCompiler
+
+    match "templates/*" $
+        compile templateBodyCompiler
---
+++ contact.md
@@ -0,0 +1,7 @@
+---
+title: Contact me
+author: Vikram
+date: 2022-06-13
+---
+
+Contact me at <vikramaunnikrishnan@gmail.com>.
